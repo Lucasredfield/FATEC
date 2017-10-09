05a5339350916eefcf47f265247f87bfdbd3d32c
Lucasredfield <31598407+Lucasredfield@users.noreply.github.com>
2017-10-09 12:32:25 -0300

    Atualizacao
    10.09

--- NetBeansProjects/Java/09.21 - Exercicios V.2/src/proposta01/Exer04.java
+++ NetBeansProjects/Java/09.21 - Exercicios V.2/src/proposta01/Exer04.java
@@ -5,12 +5,38 @@
  */
 package proposta01;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author lucas
  */
 public class Exer04 {
     public static void x(){
-        
+    int pernas = Integer.parseInt(JOptionPane.showInputDialog("Insira a"+
+        " quantidade de patas para verificar que tipo de Animal se trata: "));
+    String tipo;
+    switch(pernas){
+        case 1:
+            tipo = "Saci";
+            break;
+        case 2:
+            tipo = "Bípede";            
+            break;
+        case 3:
+            tipo = "Tripé";
+            break;
+        case 4:
+            tipo = "Quadrúpede";
+            break;
+        case 6:
+            tipo = "Aranha";            
+            break;
+        default:
+            tipo = "ET";            
+            break;
     }
+    JOptionPane.showMessageDialog(null,"Isso é um(a): " + tipo);  
+    }
+    
 }
